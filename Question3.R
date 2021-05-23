34af72c96c6a53a57508f246f73fa39fec9e7eec
katbeescience <36936435+katbeescience@users.noreply.github.com>
2021-05-23 12:14:46 -0700

    Remove redundant lines and comments.

--- Question3.R
+++ Question3.R
@@ -14,10 +14,10 @@ peckcountsonly <- read.csv(file="Data/peckcountsonly.csv")
 
 #===============================================================================
 # (3) Do carpenter bees that place their entrances lower relative to leaf
-# heights avoid predation more?
+# heights avoid predation more? Does entrance height predict number of pecks?
 #===============================================================================
 
-# Test to see if nests with no predation had a lower median height than those
+# Test whether nests with no predation had a lower median height than those
 # with predation.
 # The non-parametric test that accomplishes this is a Wilcoxon-Mann-Whitney test.
 # Null predicts that the entrance heights relative to leaf heights for the two
@@ -35,33 +35,15 @@ no.peck.ent.leaf <- peckcountsonly$entleaf[which(peckcountsonly$peck.count == 0)
 wilcox.ent.ht <- wilcox.test(x=w.peck.ent.leaf, y=no.peck.ent.leaf, paired=FALSE)
 wilcox.ent.ht
 
-# Here we use a correlation method to look for the strength of the relationship
-# between height of the entrance and the frequency of pecks.
-
-cor(peckcountsonly$entleaf,
-    peckcountsonly$peck.count, method = c("pearson"))
-
-# Here's a logistic regression, which uses 0, 1 response varibles.
-# We need to get the peck count to be interpreted as a yes/no binary variable.
-# Now build a logisitic regression that looks at whether nests with no pecks had lower entrances
-# than nests with no pecks.
-
-peckcountsonly$peck.binary <- ifelse((peckcountsonly$peck.count==0), 0, 1)
-log.reg.ent.ht <- glm(peckcountsonly$peck.binary~peckcountsonly$entleaf,
-                      family=binomial(link='logit'))
-summary(log.reg.ent.ht)
-
 # Preliminary boxplot of entrance heights with and without pecks.
 
 ggplot(peckcountsonly, aes(x=factor(peck.binary),
                            y=entleaf)) +
   geom_boxplot()
 
-# Looking at this another way, does entrance height relative to leaf height
-# predict number of pecks?
+# Does entrance height relative to leaf height predict number of pecks?
 
 non.zero.unique <- peckcountsonly[which(peckcountsonly$peck.binary == 1),]
-zero.unique <- peckcountsonly[which(peckcountsonly$peck.binary == 0),]
 
 lm <- lm(non.zero.unique$peck.count~non.zero.unique$entleaf)
 summary(lm)
@@ -72,28 +54,6 @@ abline(lm)
 # that our variance is homogenous.
 
 hetero <- ols_test_score(lm)
-hetero
-
-# Compare mean entrance heights, with/without pecks using WMW.
-
-median(zero.unique$entleaf)
-median(non.zero.unique$entleaf)
-wilcox.test(zero.unique$entleaf, non.zero.unique$entleaf)
-
-# Compare peck height relative to leaf height distribution against entrance
-# height relative to leaf distribution.
-# It's not appropriate to use a KS test for this,
-# because of the presence of ties.
-# We also cannot use a Chi-square test because the sample sizes are different.
-# Here is a Mann-Whitney U Test for independent things.
-# The two distributions are indexed through peckcountsonly$entleaf and
-# peckcountsonly$peckleaf
-
-peck.leaf <- peckdist$peckleaf
-ent.leaf <- peckdist$entleaf
-
-wilcox.test(peck.leaf,ent.leaf)
-ks.test(peck.leaf, ent.leaf)
 
 # Make an output file:
 
